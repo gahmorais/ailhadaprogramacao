b7b89a1689a301729dca06db495502ac750efca0
Gabriel Morais <gabriel@backupautomacao.com.br>
2021-05-19 08:09:28 -0300

    Implementando busca de pokemons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
+import { getAllPokemons } from "./api/Api";
+
 function App() {
+  const [pokemons, setPokemons] = useState("");
+
+  useEffect(() => {
+    async function setAllPokemons() {
+      setPokemons(await getAllPokemons());
+    }
+
+    setAllPokemons();
+  }, []);
+
+  console.log(pokemons.results);
 
   return (
     <div className="App">
-      <h1>Pokedex</h1>
+    <h1>Pokedex</h1>
+      {pokemons && pokemons.results.map( pokemon => <div>{`${pokemon.name} - ${pokemon.url}`}</div>)}
     </div>
   );
 }
--- src/api/Api.js
+++ src/api/Api.js
@@ -1,1 +1,8 @@
 import axios from "axios";
+
+export async function getAllPokemons() {
+  const allPokemons = await axios
+    .get("https://pokeapi.co/api/v2/pokemon?limit=100&offset=200")
+    .then((response) => response.data)
+  return allPokemons;
+}
