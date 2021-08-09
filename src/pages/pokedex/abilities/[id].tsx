21468cbe00066ece5ece8af60531439ec4996659
Gabriel Morais <gmdeveloper01@gmail.com>
2021-08-09 13:05:11 -0300

    Melhoria da página de tipo e da página de habilidades

--- src/pages/pokedex/abilities/[id].tsx
+++ src/pages/pokedex/abilities/[id].tsx
@@ -1,8 +1,11 @@
 import { useRouter } from "next/dist/client/router";
+import Head from "next/head";
 import { useQuery } from "react-query";
 import Loading from "../../../components/Loading";
 import Navbar from "../../../components/Navbar";
 import Pokemon from "../../../components/Pokemon";
+import Title from "../../../components/Title";
+import { capitalFirstLetter } from "../../../helpers/formatString";
 import { getAbilitiesById } from "../../../service/Api";
 
 export default function AbilitiesInfo() {
@@ -19,21 +22,40 @@ export default function AbilitiesInfo() {
     return <Loading />;
   }
 
-  const effect = abilities.effect_entries.find(
-    (effect) => effect.language.name === "en"
-  );
-  console.log(effect);
+  const { name, pokemon, effect_entries } = abilities;
+
+  const effect = effect_entries.find((effect) => effect.language.name === "en");
 
   return (
     <>
+      <Head>
+        <title>Pokedex | {capitalFirstLetter(name)}</title>
+        <link rel="icon" href="/pokebola.ico" />
+      </Head>
       <Navbar />
-      <div>{abilities.name}</div>
-      <div>{effect?.effect}</div>
-      <div className="flex flex-wrap">
-        {abilities.pokemon.map(({ pokemon }, index) => (
-          <Pokemon key={index}>{pokemon.url}</Pokemon>
-        ))}
-      </div>
+      <main className="container mx-auto flex flex-col items-center justify-center">
+        <div className="w-full text-center">
+          <span className="text-3xl font-semibold text-gray-500">
+            {capitalFirstLetter(name)} -
+          </span>
+          <span className="text-3xl font-semibold text-gray-500">
+            Nº {id.toString().padStart(3, "0")}
+          </span>
+        </div>
+        <div className={`border-2 text-lg rounded-lg w-5/6 p-4 mt-2 mb-2 hover:bg-gray-400 hover:text-white`}>
+          <span>{effect?.effect}</span>
+        </div>
+        <div className="flex flex-col mx-auto justify-center text-center">
+          <Title>Pokemons que podem adquirir esta habilidade</Title>
+          <ul className="flex flex-wrap mt-2 items-center justify-center">
+            {pokemon.map(({ pokemon }, index) => (
+              <li key={index}>
+                <Pokemon>{pokemon.url}</Pokemon>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </main>
     </>
   );
 }
--- src/pages/pokedex/types/[id].tsx
+++ src/pages/pokedex/types/[id].tsx
@@ -1,6 +1,10 @@
 import { useRouter } from "next/dist/client/router";
+import Head from "next/head";
 import { useQuery } from "react-query";
 import Loading from "../../../components/Loading";
+import Navbar from "../../../components/Navbar";
+import Title from "../../../components/Title";
+import { capitalFirstLetter } from "../../../helpers/formatString";
 import { getTypeById } from "../../../service/Api";
 
 export default function TypesInfo() {
@@ -17,5 +21,64 @@ export default function TypesInfo() {
   }
 
   const { name } = type;
-  return <div>{name}</div>;
+
+  return (
+    <div className={`bg-${name} flex flex-col h-screen overflow-auto`}>
+      <Navbar />
+      <Head>
+        <title>Pokedex | {capitalFirstLetter(name)}</title>
+        <link rel="icon" href="/pokebola.ico" />
+      </Head>
+      <main className="flex flex-col container mx-auto items-center">
+        <div className="flex flex-wrap space-x-2 m-4 text-center">
+          <span className="text-3xl font-semibold text-white">
+            {capitalFirstLetter(name)}
+          </span>
+          <span className="text-3xl font-semibold text-white"> - </span>
+          <span className="text-3xl font-semibold text-white">
+            Nº {id.toString().padStart(3, "0")}
+          </span>
+        </div>
+        <Title>Relação de dano</Title>
+        <div className="flex flex-wrap mx-auto justify-center">
+          <div className="flex flex-col w-50 h-30 items-center border-white border-2 m-2 text-white rounded-lg p-4">
+            <span>Dano duplo de:</span>
+            {type.damage_relations.double_damage_from.map((damage) => {
+              return <span>{capitalFirstLetter(damage.name)}</span>;
+            })}
+          </div>
+          <div className="flex flex-col w-50 h-30 items-center border-white border-2 m-2 text-white rounded-lg p-4">
+            <span>Dano duplo em: </span>
+            {type.damage_relations.double_damage_to.map((damage) => {
+              return <span>{capitalFirstLetter(damage.name)}</span>;
+            })}
+          </div>
+          <div className="flex flex-col w-50 h-30 items-center border-white border-2 m-2 text-white rounded-lg p-4">
+            <span>Metade do dano de: </span>
+            {type.damage_relations.half_damage_from.map((damage) => {
+              return <span>{capitalFirstLetter(damage.name)}</span>;
+            })}
+          </div>
+          <div className="flex flex-col w-50 h-30 items-center border-white border-2 m-2 text-white rounded-lg p-4">
+            <span>Metade do dano em: </span>
+            {type.damage_relations.half_damage_to.map((damage) => {
+              return <span>{capitalFirstLetter(damage.name)}</span>;
+            })}
+          </div>
+          <div className="flex flex-col w-50 h-30 items-center border-white border-2 m-2 text-white rounded-lg p-4">
+            <span>Nenhum dano de: </span>
+            {type.damage_relations.no_damage_from.map((damage) => {
+              return <span>{capitalFirstLetter(damage.name)}</span>;
+            })}
+          </div>
+          <div className="flex flex-col w-50 h-30 items-center border-white border-2 m-2 text-white rounded-lg p-4">
+            <span>Nenhum dano em: </span>
+            {type.damage_relations.no_damage_to.map((damage) => {
+              return <span>{capitalFirstLetter(damage.name)}</span>;
+            })}
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
